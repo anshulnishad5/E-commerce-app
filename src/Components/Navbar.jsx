364f269093ac4beccdd12fecf24e4d8c267effb3
anshul <anshulnishad525@gmail.com>
2026-01-28 19:00:43 +0530

    Add send-invoice API with Resend email integration

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,45 +1,36 @@
 import { Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { logout } from "../redux/authSlice";
-import "../index.css";
-import logo from "../assets/images/shopeasy_logo_orange_transparent.svg";
+import logo from "../assets/images/shopeasy-logo.svg";
 
 function Navbar ()
 {
     const dispatch = useDispatch();
-    const cart = useSelector( state => state.cart.items );
-    const user = useSelector( state => state.auth.user );
+    const cart = useSelector( ( state ) => state.cart.items );
+    const user = useSelector( ( state ) => state.auth.user );
 
     return (
         <nav className="navbar">
-            {/* LEFT: Logo + Brand */ }
-            {/* LEFT: Logo + Brand */ }
-            <div className="flex items-center gap-2 bg-slate-850 px-3 py-2 rounded-md">
+            {/* LEFT: LOGO */ }
+            <Link to="/" className="flex items-center h-full">
                 <img
                     src={ logo }
                     alt="ShopEasy Logo"
-                    className="w-8 h-8 object-contain"
+                    className="h-34 w-auto object-contain transition-transform duration-300 hover:scale-105"
                 />
-
-                <h1 className="text-xl font-bold text-white leading-none">
-                    ShopEasy
-                </h1>
-            </div>
+            </Link>
 
 
-
-
-
-
-            <ul>
+            {/* RIGHT: LINKS */ }
+            <ul className="flex items-center gap-4">
                 <li><Link to="/">Home</Link></li>
                 <li><Link to="/products">Products</Link></li>
-                <li><Link to="/cart">🛒Cart ({ cart.length })</Link></li>
+                <li><Link to="/cart">🛒 Cart ({ cart.length })</Link></li>
 
                 { user ? (
                     <>
                         <li><Link to="/orders">My Orders</Link></li>
-                        <li>{ user.email }</li>
+                        <li className="text-sm opacity-80">{ user.email }</li>
                         <li
                             className="logout"
                             onClick={ () => dispatch( logout() ) }
