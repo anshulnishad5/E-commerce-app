a0d65cd1308b19b714e1814249d0f97ec35a0b86
anshulnishad5 <anshulnishad525@gmail.com>
2026-02-12 11:29:10 +0530

    Fix className attribute in Home.jsx

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -51,7 +51,7 @@ function Home ()
             { productGroups.map( ( group, index ) => (
                 <section
                     key={ index }
-                    className=className="max-w-7xl mx-auto px-6 mb-16"
+                    className="max-w-7xl mx-auto px-6 mb-16"
 
                 >
                     <div className="flex items-center justify-between mb-6">
